52683cf4dda431f0a3e9dc2b52f26f6f28d732a2
Matheus Gabriel <math.gsilva07@gmail.com>
2026-05-16 16:47:12 +0000

    Refactor code structure for improved readability and maintainability

---
+++ Meu-Dindin/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Meu_Dindin.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
